06b424132ddf02b368502695454218d7f56b9172
Rasih CAGLAYAN <rasihcaglayan@derinbilgi.com.tr>
2019-07-28 21:38:24 +0300

    updates cryptoMarket product view model
    updates cryptoMarket product view model

--- src/CryptoMarket.Api/Application/Services/ProductGateway/ProductViewModel.cs
+++ src/CryptoMarket.Api/Application/Services/ProductGateway/ProductViewModel.cs
@@ -7,7 +7,7 @@ namespace CryptoMarket.Api.Application.Services.ProductGateway
         public int Id { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
-        public PriceUnits PriceUnit { get; set; }
+        public string PriceUnit { get; set; }
         
     }
 }

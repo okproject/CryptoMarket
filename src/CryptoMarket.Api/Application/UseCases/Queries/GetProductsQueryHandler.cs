4081afc7e561158476c212d53a129497823078a3
Rasih CAGLAYAN <rasihcaglayan@derinbilgi.com.tr>
2019-07-28 21:36:43 +0300

    adds model conversion to product gateway
    adds model conversion to product gateway concrete implementation

--- src/CryptoMarket.Api/Application/UseCases/Queries/GetProductsQueryHandler.cs
+++ src/CryptoMarket.Api/Application/UseCases/Queries/GetProductsQueryHandler.cs
@@ -1,11 +1,14 @@
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using CryptoMarket.Api.Application.Abstraction;
 using CryptoMarket.Api.Application.Services.ProductGateway;
+using CryptoMarket.Api.Core.Model;
 
 namespace CryptoMarket.Api.Application.UseCases.Queries
 {
-    public class GetProductsQueryHandler:IQueryHandler<GetProductsQuery,ProductViewModel>
+    public class GetProductsQueryHandler:IQueryHandler<GetProductsQuery,List<ProductViewModel>>
     {
         private IProductGateway _productGateway;
 
@@ -14,9 +17,18 @@ namespace CryptoMarket.Api.Application.UseCases.Queries
             _productGateway = productGateway;
         }
 
-        public async Task<ProductViewModel> Handle(GetProductsQuery query, CancellationToken token)
+        public async Task<List<ProductViewModel>> Handle(GetProductsQuery query, CancellationToken token)
         {
-            throw new System.NotImplementedException();
+            var productList = await _productGateway.GetProductsQuery(new CancellationToken(), query.Start, query.Size);
+
+            var productViewModelList = productList?.data?.Select(x => new ProductViewModel()
+            {
+                Id = x.id,
+                Name = x.name,
+                Price = x.quote.USD.price,
+                PriceUnit = PriceUnits.USD.ToString()
+            }).ToList();
+            return productViewModelList;
         }
     }
 }

8071c089c6cc68c313f7d4e0feaafd908470f912
Rasih CAGLAYAN <rasihcaglayan@derinbilgi.com.tr>
2019-07-28 22:15:23 +0300

    adds and updates migration and data related process
    adds and updates migration and data related process

--- src/CryptoMarket.Api/Infrastructure/Data/Migrations/20190728190905_adds Purchase.Designer.cs
+++ src/CryptoMarket.Api/Infrastructure/Data/Migrations/20190728190905_adds Purchase.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using CryptoMarket.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CryptoMarket.Api.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190728190905_adds Purchase")]
+    partial class addsPurchase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/CryptoMarket.Api/Infrastructure/Data/Migrations/20190728190905_adds Purchase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CryptoMarket.Api.Data.Migrations
+{
+    public partial class addsPurchase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Purchases",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CustomerId = table.Column<int>(nullable: false),
+                    ProductId = table.Column<int>(nullable: false),
+                    Price = table.Column<decimal>(nullable: false),
+                    PriceUnit = table.Column<string>(nullable: true),
+                    Amount = table.Column<double>(nullable: false),
+                    CreateDateTime = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Purchases", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Purchases");
+        }
+    }
+}

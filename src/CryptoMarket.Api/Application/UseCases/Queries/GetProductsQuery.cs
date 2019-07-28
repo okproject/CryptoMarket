08a1c58f3ec00dd58e0f7a1d7c5bf8f0a26f3ace
Rasih CAGLAYAN <rasihcaglayan@derinbilgi.com.tr>
2019-07-28 17:51:15 +0300

    adds get products query
    adds get products query

--- src/CryptoMarket.Api/Application/UseCases/Queries/GetProductsQuery.cs
+++ src/CryptoMarket.Api/Application/UseCases/Queries/GetProductsQuery.cs
@@ -2,6 +2,7 @@ namespace CryptoMarket.Api.Application.UseCases.Queries
 {
     public class GetProductsQuery
     {
-        
+        public int Start { get; set; }
+        public int Size { get; set; }
     }
 }

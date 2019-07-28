2ebfbc777a86823e3e027caf64cbb673f449bbeb
Rasih CAGLAYAN <rasihcaglayan@derinbilgi.com.tr>
2019-07-28 17:52:15 +0300

    adds concrete purchase repository
    adds concrete purchase repository

---
+++ src/CryptoMarket.Api/Infrastructure/Data/Repository/PurchaseRepository.cs
@@ -0,0 +1,19 @@
+using System.Threading.Tasks;
+using CryptoMarket.Api.Core;
+using CryptoMarket.Api.Core.Model;
+
+namespace CryptoMarket.Api.Data.Repository
+{
+    public class PurchaseRepository:IPurchaseRepository
+    {
+        public async Task Remove(Purchase entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task Add(Purchase entity)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

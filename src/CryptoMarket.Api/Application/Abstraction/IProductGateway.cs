fd0a0110c49c8389b50649204a13a20bcf2a6fc2
Rasih CAGLAYAN <rasihcaglayan@derinbilgi.com.tr>
2019-07-28 23:33:58 +0300

    adds get product price by id
    adds get product price by id

--- src/CryptoMarket.Api/Application/Abstraction/IProductGateway.cs
+++ src/CryptoMarket.Api/Application/Abstraction/IProductGateway.cs
@@ -9,5 +9,7 @@ namespace CryptoMarket.Api.Application.Abstraction
     public interface IProductGateway
     {
         Task<ProductCoinMarketCapViewModel> GetProductsQuery(CancellationToken cancellationToken,int? start=1, int? limit=20);
+
+        Task<decimal> GetProductPriceById(CancellationToken cancellationToken,int id);
     }
 }
--- src/CryptoMarket.Api/Infrastructure/Service/ProductGateway.cs
+++ src/CryptoMarket.Api/Infrastructure/Service/ProductGateway.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading;
@@ -7,6 +8,8 @@ using System.Threading.Tasks;
 using CryptoMarket.Api.Application.Abstraction;
 using CryptoMarket.Api.Application.Services.ProductGateway;
 using Marvin.StreamExtensions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace CryptoMarket.Api.Infrastructure.Service
 {
@@ -20,7 +23,8 @@ namespace CryptoMarket.Api.Infrastructure.Service
         }
 
 
-        public async Task<ProductCoinMarketCapViewModel> GetProductsQuery(CancellationToken cancellationToken, int? start,
+        public async Task<ProductCoinMarketCapViewModel> GetProductsQuery(CancellationToken cancellationToken,
+            int? start,
             int? limit)
         {
             if (start == null || start <= 0) start = 1;
@@ -41,6 +45,34 @@ namespace CryptoMarket.Api.Infrastructure.Service
                 response.EnsureSuccessStatusCode();
                 var result = stream.ReadAndDeserializeFromJson<ProductCoinMarketCapViewModel>();
                 return result;
+            }
+        }
+
+        public async Task<decimal> GetProductPriceById(CancellationToken cancellationToken, int id)
+        {
+            if (id < 0) return 0;
+            var requestUri = $"v1/cryptocurrency/quotes/latest?convert=USD&id={id}";
+            var request = new HttpRequestMessage(
+                HttpMethod.Get,
+                requestUri);
+            request.Headers.AcceptEncoding.Add(new StringWithQualityHeaderValue("gzip"));
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+
+            using (var response = await _httpClient.SendAsync(request,
+                HttpCompletionOption.ResponseHeadersRead,
+                cancellationToken))
+            {
+                var resStr = await response.Content.ReadAsStringAsync();
+                var jsonResponse = JObject.Parse(resStr);
+                var priceJObject = jsonResponse["data"]?[$"{id.ToString()}"]?["quote"]?["USD"]?["price"];
+                if(priceJObject==null) throw new Exception("Could not get price information");
+                var priceStr = priceJObject.ToString();
+                if(string.IsNullOrEmpty(priceStr)) throw new Exception("Could not get price information");
+                response.EnsureSuccessStatusCode();
+                var parseResult = decimal.TryParse(priceStr, out var decimalResult);
+                if (!parseResult) throw new Exception("Could not parse price information");
+                return decimalResult;
             }
         }
     }

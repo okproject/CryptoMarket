8fa4e0090cdd3286532747a7c21f652614f63501
Rasih CAGLAYAN <rasihcaglayan@derinbilgi.com.tr>
2019-07-28 17:53:03 +0300

    adds market ctrl method prototypes
    adds market ctrl method prototypes

--- src/CryptoMarket.Api/Presentation/Controllers/Market.cs
+++ src/CryptoMarket.Api/Presentation/Controllers/Market.cs
@@ -1,3 +1,9 @@
+using System.Threading;
+using System.Threading.Tasks;
+using CryptoMarket.Api.Application.Abstraction;
+using CryptoMarket.Api.Application.UseCases.Commands;
+using CryptoMarket.Api.Application.UseCases.Queries;
+using CryptoMarket.Api.Core;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CryptoMarket.Api.Controllers
@@ -6,7 +12,31 @@ namespace CryptoMarket.Api.Controllers
     [Route("api/[Controller]")]
     public class MarketController:ControllerBase
     {
-        
-        
+        private readonly IProductGateway _productGateway;
+        private readonly IPurchaseRepository _purchaseRepository;
+
+        public MarketController(IProductGateway productGateway, IPurchaseRepository purchaseRepository)
+        {
+            _productGateway = productGateway;
+            _purchaseRepository = purchaseRepository;
+        }
+
+        [HttpGet]
+        [Route("products")]
+        public async Task<IActionResult> Products([FromQuery]GetProductsQuery query)
+        {
+            var productQueryHandler = new GetProductsQueryHandler(_productGateway);
+            var result = await productQueryHandler.Handle(query, new CancellationToken());
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("purchase")]
+        public async Task<IActionResult> PurchaseProduct([FromBody] PurchaseProductCommand command)
+        {
+            var handler=new PurchaseProductCommandHandler(_productGateway,_purchaseRepository);
+            await handler.Handle(command,new CancellationToken());
+            return StatusCode(201);
+        }
     }
 }

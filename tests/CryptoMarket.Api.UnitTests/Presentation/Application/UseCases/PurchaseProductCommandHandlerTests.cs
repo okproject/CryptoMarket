e0a27a5f566fac975416ac9d3b255cf7b924fbde
Rasih CAGLAYAN <rasihcaglayan@derinbilgi.com.tr>
2019-07-29 04:45:28 +0300

    Create PurchaseProductCommandHandlerTests
    Create PurchaseProductCommandHandlerTests

---
+++ tests/CryptoMarket.Api.UnitTests/Presentation/Application/UseCases/PurchaseProductCommandHandlerTests.cs
@@ -0,0 +1,50 @@
+using System.Threading;
+using System.Threading.Tasks;
+using CryptoMarket.Api.Application.Services.ProductGateway;
+using CryptoMarket.Api.Application.UseCases.Commands;
+using CryptoMarket.Api.Core;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace CryptoMarket.Api.UnitTests.Presentation.Application.UseCases
+{
+    public class PurchaseProductCommandHandlerTests
+    {
+        public PurchaseProductCommandHandlerTests()
+        {
+            
+        }
+
+        [Fact]
+        public async Task Shoud_Calculate_Total()
+        {
+            var prdGtwy = new Mock<IProductGateway>();
+            var purRepo = new Mock<IPurchaseRepository>();
+            
+            var price = 0.22m;
+            var amount = 0.522; //0,11484
+            var command = new PurchaseProductCommand()
+            {
+                Amount = amount,
+                ProductId = 1
+            };
+
+
+            prdGtwy.Setup(x => x.GetProductPriceById(It.IsAny<CancellationToken>(), It.IsAny<int>()))
+                .Returns(Task.FromResult(price));
+
+            var handler = new PurchaseProductCommandHandler(prdGtwy.Object, purRepo.Object);
+
+            await handler.Handle(command, new CancellationToken());
+            prdGtwy.Verify(x=>x.GetProductPriceById(It.IsAny<CancellationToken>(),It.IsAny<int>()),Times.Once);
+
+            var purchaseModelResult = await handler.CreateAsync(command);
+            var calculationResult= handler.CalculateTotal(command.Amount, price);
+            purchaseModelResult.TotalPrice.Should().Equals(calculationResult);
+
+
+
+        }
+    }
+}
